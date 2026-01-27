0203cf62d7214fe79b42a804fc51111e805f2afd
Eric Ahn <w@chill.dev>
2026-01-27 13:21:25 -0800

    feat(Reddit): Add support for Reddit `2026.04.0` (#276)
    * Add RVX reddit extension code
    
    * Add RVX reddit patches
    
    * Add temporary proguard keep to fix unexplained stripping of kotlin null check method
    
    * Add spoof extension classes
    
    * fix extension fingerprints cannot be found
    
    * merge existing resource together with RVX ResourceUtils
    
    * Add Morphe about screen to settings
    
    * Update branding
    
    * Add import/export preference
    
    * refactor
    
    * Mark 2026.04.0 as supported
    
    * Replace PatchList and move patch status into individual patches, inlined some constants
    
    * Keep old code compiling but disabled until it can be fixed
    
    * Convert most fingerprints to use instruction filters. More complex usage can wait until the next time the patch is touched
    
    * Verify extension method exists during patching
    
    * refactor
    
    * add placeholder RVX universal patches (similar patches were previously in this repo but was deleted). RVX has additional changes and fixes not present in the original RV merged code.
    
    * Fix navigation bar patch and allow disabling of Answers button
    
    * format indentation, no functional changes
    
    * refactor
    
    * refactor
    
    * remove unused library
    
    * update min sdk to match oldest supported Reddit (Android 9)
    
    * fix minSdk
    
    * Remove unused code, add attribution source
    
    * Some additional settings requires a restart
    
    * fix typo
    
    ---------
    
    Co-authored-by: LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>

---
+++ extensions/reddit/src/main/java/app/morphe/extension/reddit/patches/NavigationButtonsPatch.java
@@ -0,0 +1,75 @@
+package app.morphe.extension.reddit.patches;
+
+import android.content.res.Resources;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import app.morphe.extension.reddit.settings.Settings;
+import app.morphe.extension.shared.Logger;
+import app.morphe.extension.shared.ResourceType;
+import app.morphe.extension.shared.ResourceUtils;
+import app.morphe.extension.shared.Utils;
+
+@SuppressWarnings("unused")
+public final class NavigationButtonsPatch {
+    private static Resources mResources;
+    private static final Map<Object, String> navigationMap = new LinkedHashMap<>(NavigationButton.values().length);
+    private static final Map<String, String> labelMap = new HashMap<>();
+
+    public static void setResources(Resources resources) {
+        mResources = resources;
+    }
+
+    /**
+     * @return If this patch was included during patching.
+     */
+    public static boolean isPatchIncluded() {
+        return false;  // Modified during patching.
+    }
+
+    public static void mapResourceId(int id) {
+        String resourceName = mResources.getResourceEntryName(id);
+        String label = mResources.getString(id);
+        labelMap.put(label, resourceName);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static void setNavigationMap(Object object, String label) {
+        String labelName = labelMap.get(label);
+        for (NavigationButton button : NavigationButton.values()) {
+            if (button.label.equals(labelName) && button.enabled) {
+                navigationMap.putIfAbsent(object, label);
+            }
+        }
+    }
+
+    public static void hideNavigationButtons(List<Object> list, Object object) {
+        if (list != null && !navigationMap.containsKey(object)) {
+            list.add(object);
+        }
+    }
+
+    private enum NavigationButton {
+        ANSWERS(Settings.HIDE_ANSWERS_BUTTON.get(),  "answers_label"),
+        CHAT(Settings.HIDE_CHAT_BUTTON.get(),  "label_chat"),
+        CREATE(Settings.HIDE_CREATE_BUTTON.get(),  "action_create"),
+        DISCOVER(Settings.HIDE_DISCOVER_BUTTON.get(),  "communities_label");
+        private final boolean enabled;
+        private final String label;
+
+        NavigationButton(final boolean enabled, final String label) {
+            this.enabled = enabled;
+            this.label = label;
+        }
+    }
+}
